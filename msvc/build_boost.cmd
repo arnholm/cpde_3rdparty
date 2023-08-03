5502c2b19b45b82f222e483e48e43d76b067562a
Carsten Arnholm <arnholm@arnholm.org>
2023-08-03 15:38:52 +0200

    boost 1.82 with python 3.11

--- msvc/build_boost.cmd
+++ msvc/build_boost.cmd
@@ -1,10 +1,15 @@
 @echo off
 REM  This procedure builds static boost libraries for Windows/MSVC
 REM  Procedure must be run from "Visual Studio Command Prompt"
+REM  Make sure
+REM     - PYTHON_DIR environment variable points to the current dir for 
+REM       python version you want to link with 
+REM     - User PATH refers to %PYTHON_DIR% and %PYTHON_DIR%\Scripts (b2 appears to use this)
+REM     - 'using python' below refers to same Python version number indicated by PYTHON_DIR
 REM
 mkdir ..\msvclibs
 pushd ..\msvclibs
-git clone --recursive https://github.com/boostorg/boost.git  --branch boost-1.74.0  --single-branch
+git clone --recursive https://github.com/boostorg/boost.git  --branch boost-1.82.0  --single-branch
 pushd boost
 REM
 call bootstrap.bat vc142
@@ -14,7 +19,13 @@ REM Set address-model=64 for x86_amd64 cross compiler or native amd64 build
 REM Set architecture=x86 in all cases
 REM
 REM Make sure the correct version of python is found
-echo using python : 3.8 : D:\\cpde_dep\\3rdparty\\python\\python38 : ; > user-config.jam
+REM
+REM replace backslashes in path with forward slashes to avoid 
+REM problems in user-config.jam (backslash is escape character)
+set "PYTHON_DIR=%PYTHON_DIR:\=/%"
+REM
+REM echo using python : 3.11 : C:/source/Python/Python311 : ; > user-config.jam
+echo using python : 3.11 : %PYTHON_DIR% : ; > user-config.jam
 REM 
 .\b2 -j8 toolset=msvc  --user-config=user-config.jam address-model=64 architecture=x86 link=static threading=multi runtime-link=shared --build-type=minimal 
 REM return to the directory of this batch file
